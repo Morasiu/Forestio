722316921be601ed1f1c09ca92e3fe2ad75d465a
Hubert Morawski <morasiu2@gmail.com>
2020-01-31 22:29:22 +0100

    RotationManager

---
+++ Forestio/Assets/RotationManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    [SerializeField]
+    private float rotSpeed = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse2)) {
+            float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+            float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+            transform.RotateAround(Vector3.up, -rotX);
+            transform.RotateAround(Vector3.right, rotY);
+        }
+    }
+}
