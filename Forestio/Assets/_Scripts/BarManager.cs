a5466efd109af1ad77ffff406db6b8255b24a868
DawidSieczka <dawid.sieczka@pirum-technology.com>
2020-01-31 22:26:40 +0100

    created progres bar UI

---
+++ Forestio/Assets/_Scripts/BarManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarManager : MonoBehaviour
+{
+    Hexa HexaList;
+    void Start()
+    {
+        HexaList = FindObjectOfType<Hexa>();
+    }
+}
---
+++ Forestio/Assets/_Scripts/ProgressBar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public static Image o2Bar { get; set; }
+
+    public static void ChangeValue(int value)
+    {
+        o2Bar.rectTransform.sizeDelta = new Vector2(value, 0);
+    }
+}
