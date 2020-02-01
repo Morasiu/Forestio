a93e778cb28e752c3aacc1900109f6dc0b12abcd
Hubert Morawski <morasiu2@gmail.com>
2020-02-01 07:59:25 +0100

    no message

---
+++ Forestio/Assets/TestRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var parent = transform.parent;
+        transform.position = parent.position + parent.transform.forward * 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
