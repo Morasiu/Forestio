f80952b0f2c1ca5badda84aee33767e7ae24d3a7
WSkrzybalski <w.skrzybalski@interia.pl>
2020-02-01 01:16:14 +0100

    world prefab

--- Forestio/Assets/_Script/RotationManager.cs
+++ Forestio/Assets/_Script/RotationManager.cs
@@ -9,11 +9,15 @@ public class RotationManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        print($"x: {Input.GetAxis("Mouse X")}");
+        print($"y: {Input.GetAxis("Mouse Y")}");
+
         if (Input.GetKey(KeyCode.Mouse2)) {
             float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
             float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
 
-            transform.RotateAround(Vector3.up, -rotX);
+            transform.RotateAround(Vector3.up, rotX);
             transform.RotateAround(Vector3.right, rotY);
         }
     }
