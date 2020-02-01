fe4f288c0376acd7bcea4bc9b4810331af6dbe2e
Hubert Morawski <morasiu2@gmail.com>
2020-02-01 12:00:01 +0100

    Rotator

---
+++ Forestio/Assets/ObjectRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(2 * transform.position - Vector3.zero);
+        //transform.localRotation = Quaternion.Euler(new Vector3(
+        //    transform.localRotation.x = 90f, 
+        //    transform.localRotation.y, 
+        //    transform.localRotation.z));
+    }
+
+    
+}
