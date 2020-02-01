10233c45bf664e490def06120042924334a666bd
Hubert Morawski <morasiu2@gmail.com>
2020-02-01 08:18:56 +0100

    FactoryAI

--- Forestio/Assets/_Script/Hex.cs
+++ Forestio/Assets/_Script/Hex.cs
@@ -14,22 +14,22 @@ public class Hex : MonoBehaviour
         Neighbours = new Dictionary<NeighbourDirection, GameObject>();
         HexState = HexState.Neutral;
 
-        for (int i = 0; i < Enum.GetValues(typeof(NeighbourDirection)).Length; i++) {
-            Vector3 direction = transform.TransformDirection(Vector3.back) * 5;
-            if (Physics.Raycast(transform.position, direction, out var hit, 20)) {
-                Debug.Log("Hit: " + hit.collider.name);
-            }
-        }
+        //for (int i = 0; i < Enum.GetValues(typeof(NeighbourDirection)).Length; i++) {
+        //    Vector3 direction = transform.TransformDirection(Vector3.back) * 5;
+        //    if (Physics.Raycast(transform.position, direction, out var hit, 20)) {
+        //        Debug.Log("Hit: " + hit.collider.name);
+        //    }
+        //}
 
-        Debug.Log(Neighbours.Count);
+        //Debug.Log(Neighbours.Count);
     }
 
-    void OnDrawGizmosSelected() {
-        // Draws a 5 unit long red line in front of the object
-        Gizmos.color = Color.red;
-        Vector3 direction = transform.TransformDirection(Vector3.back) * 5;
-        Gizmos.DrawRay(transform.position, direction * 5);
-    }
+    //void OnDrawGizmosSelected() {
+    //    // Draws a 5 unit long red line in front of the object
+    //    Gizmos.color = Color.red;
+    //    Vector3 direction = transform.TransformDirection(Vector3.back) * 5;
+    //    Gizmos.DrawRay(transform.position, direction * 5);
+    //}
 
     // Update is called once per frame
     void Update()
